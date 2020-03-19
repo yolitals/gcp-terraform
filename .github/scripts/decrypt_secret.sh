d25133709c192f7c00940f517d394e2d3ab05834
yolitals <yolita2402@gmail.com>
2020-03-19 00:20:56 -0600

    add tf plan

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
 --output ${FILE_NAME} ${FILE_NAME}.gpg
