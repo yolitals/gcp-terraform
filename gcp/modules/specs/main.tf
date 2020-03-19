ce1e444379944eccb78a8be2103e382fe9c198bc
Yolanda Lopez <yolopez@paypal.com>
2020-03-19 15:42:39 -0600

    [master] Add terratest

--- gcp/modules/specs/main.tf
+++ gcp/modules/specs/main.tf
@@ -5,7 +5,7 @@ module "vm_test" {
   source          = "../compute_instance"
   project_name    = "wwcode-terraform-admin"
   region          = "us-central1"
-  instance_name   = "dev-instance"
+  instance_name   = "test-instance"
   machine_type    = "f1-micro"
   boot_image      = "ubuntu-1604-xenial-v20170328"
   target_tags     = ["web-app"]
