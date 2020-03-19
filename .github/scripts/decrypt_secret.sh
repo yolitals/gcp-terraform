d5f8fa59d07effa0cbfb87e0a8fcb34eb81d9eb7
yolitals <yolita2402@gmail.com>
2020-03-19 00:02:52 -0600

    fix actions

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,4 +4,4 @@
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/${FILE_NAME} ${FILE_NAME}.gpg
+--output ${FILE_NAME} ${FILE_NAME}.gpg
