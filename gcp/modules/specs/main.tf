e13863e0688fa62b92c3068245644ac6077e8912
yolitals <yolita2402@gmail.com>
2021-10-22 19:42:16 -0600

    Update main.tf

--- gcp/modules/specs/main.tf
+++ gcp/modules/specs/main.tf
@@ -4,7 +4,7 @@ variable "json_credential" {}
 module "vm_test" {
   source          = "../compute_instance"
   project_name    = "wwcode-terraform-admin"
-  region          = "us-central2"
+  region          = "us-central1"
   instance_name   = "test-instance"
   machine_type    = "f1-micro"
   boot_image      = "ubuntu-1604-xenial-v20170328"
